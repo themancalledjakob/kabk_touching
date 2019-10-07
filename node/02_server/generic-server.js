2bc5cd68bfe1739dbfe9aefe05e0b5ae75027fc0
themancalledjakob <asukhorukova@inbox.ru>
2019-10-07 07:32:04 +0200

    animation example

--- node/02_server/generic-server.js
+++ node/02_server/generic-server.js
@@ -8,7 +8,7 @@ var webSocketServer = require('websocket').server;
 var http = require('http');
 
 const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const Readline = require('@serialport/parser-readline');const port = new SerialPort('/dev/tty.usbmodemFD121', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
 /**
  * Global variables
